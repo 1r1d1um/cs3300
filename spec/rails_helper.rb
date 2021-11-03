62b09c42baa6207b5eb8ad8beeb4e659ab09b64c
1r1d1um <amitche3@uccs.edu>
2021-11-03 22:27:29 +0000

    added some files that should have stayed

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,7 +69,4 @@ SimpleCov.start 'rails' do
   add_filter '/db/'
   add_filter '/spec/' # for rspec
   add_filter 'app/mailers'
-  add_filter 'app/helpers'
-  add_filter 'app/jobs'
-  add_filter 'app/channels'
 end
