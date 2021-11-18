1909d335e6329962667ff57a335f0997b93d4731
1r1d1um <amitche3@uccs.edu>
2021-11-18 22:24:26 +0000

    1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 
 end
