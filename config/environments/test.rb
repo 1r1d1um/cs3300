8f07d65945df4a85f245d5bd631604e531a7f238
1r1d1um <amitche3@uccs.edu>
2021-11-19 02:01:45 +0000

    1

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system in a temporary directory
   config.active_storage.service = :test
 
-  config.action_mailer.perform_caching = true
+  config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
@@ -43,7 +43,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
 end
