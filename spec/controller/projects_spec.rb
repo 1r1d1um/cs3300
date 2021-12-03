abc1775f4ef64ef2805c5371286f0b9de2f6196c
1r1d1um <amitche3@uccs.edu>
2021-12-03 09:57:46 +0000

    Pushing to heroku

--- spec/controller/projects_spec.rb
+++ spec/controller/projects_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe ProjectsController, type: :controller do
   end
 
   context "GET #show" do
-    let!(:project) { Project.create(title: "Test title", description: "Test description") }
+    let!(:project) { Project.create(title: "Test title", description: "Test description", description2: "Test description") }
     it "returns a success response" do
       get :show, params: { id: project }
       expect(response).to be_success
